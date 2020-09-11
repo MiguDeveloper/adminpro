a605559870b32e4f8eca595dd9935600326719a2
MiguDeveloper <miguel.peru.seo@gmail.com>
2020-09-11 13:28:01 -0500

    scss agregado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { SharedModule } from './shared/shared.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,7 +7,7 @@ import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, SharedModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -6,12 +6,9 @@ import { BreadcrumbsComponent } from './breadcrumbs/breadcrumbs.component';
 import { HeaderComponent } from './header/header.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 
-
 @NgModule({
   declarations: [BreadcrumbsComponent, HeaderComponent, SidebarComponent],
-  imports: [
-    CommonModule,
-    SharedRoutingModule
-  ]
+  imports: [CommonModule, SharedRoutingModule],
+  exports: [HeaderComponent],
 })
-export class SharedModule { }
+export class SharedModule {}
